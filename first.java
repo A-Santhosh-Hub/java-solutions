7ebc14c60663142b4b5e04bab68e404b92b0d8d6
A-Santhosh-Hub <arussanthosh@gmail.com>
2025-03-07 22:25:43 +0530

    Update first.java
    Add tow input

--- first.java
+++ first.java
@@ -1,6 +1,11 @@
 import java.util.*;
 class Main{
     public static void main(String args[]){
-        System.out.println("Hello, World");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the Number ");
+        int a=in.nextInt();
+        int b=in.nextInt();
+        int c=a+b;
+        System.out.println(c);
     }
 }
